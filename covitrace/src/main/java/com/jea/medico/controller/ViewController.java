88249697b5c0e283b1273f406bb302ac1e05595d
sibin <sibin.x@jeadigital.com>
2020-09-13 22:49:57 +0530

    added initialiser and created view controller

--- covitrace/src/main/java/com/jea/medico/controller/ViewController.java
+++ covitrace/src/main/java/com/jea/medico/controller/ViewController.java
@@ -1,5 +1,14 @@
 package com.jea.medico.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 public class ViewController {
 
+	
+	 @RequestMapping(value = "/")
+	    public ModelAndView indexDirect() {
+	        ModelAndView view = new ModelAndView("index");
+	        return view;
+	    }
 }
