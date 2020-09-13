e62a4b47edea5f9bdb49a0f2eb689b34f4290b15
sibin <sibin.x@jeadigital.com>
2020-09-13 22:22:15 +0530

    added application config

---
+++ covitrace/src/main/java/com/jea/medico/WebAppConfig.java
@@ -0,0 +1,124 @@
+package com.jea.medico;
+
+import java.util.Properties;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@Configuration
+@EnableWebMvc
+@EnableTransactionManagement
+@ComponentScan("com.jea")
+@PropertySource("classpath:application.properties")
+@EnableJpaRepositories("com.jea.medico.repository")
+public class WebAppConfig {
+
+	private static final String PROPERTY_NAME_DATABASE_DRIVER = "db.driver";
+	private static final String PROPERTY_NAME_DATABASE_PASSWORD = "db.password";
+	private static final String PROPERTY_NAME_DATABASE_URL = "db.url";
+	private static final String PROPERTY_NAME_DATABASE_USERNAME = "db.username";
+
+	private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "hibernate.dialect";
+	private static final String PROPERTY_NAME_HIBERNATE_SHOW_SQL = "hibernate.show_sql";
+	private static final String PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN = "entitymanager.packages.to.scan";
+
+	@Resource
+	private Environment env;
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(env.getRequiredProperty(PROPERTY_NAME_DATABASE_DRIVER));
+		dataSource.setUrl(env.getRequiredProperty(PROPERTY_NAME_DATABASE_URL));
+		dataSource.setUsername(env.getRequiredProperty(PROPERTY_NAME_DATABASE_USERNAME));
+		dataSource.setPassword(env.getRequiredProperty(PROPERTY_NAME_DATABASE_PASSWORD));
+
+		return dataSource;
+	}
+
+	
+	@Bean
+	public JpaTransactionManager transactionManager() {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+		return transactionManager;
+	}
+	
+	
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+		entityManagerFactoryBean.setDataSource(dataSource());
+		entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+		entityManagerFactoryBean.setPackagesToScan(env.getRequiredProperty(PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN));
+		
+		entityManagerFactoryBean.setJpaProperties(hibProperties());
+		
+		return entityManagerFactoryBean;
+	}
+	
+	@Bean
+    @Autowired
+    public HibernateTransactionManager getTransaction(SessionFactory sessionFactory) {
+        HibernateTransactionManager htm = new HibernateTransactionManager();
+        htm.setSessionFactory(sessionFactory);
+        return htm;
+    }
+
+	private Properties hibProperties() {
+		Properties properties = new Properties();
+		properties.put(PROPERTY_NAME_HIBERNATE_DIALECT,	env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_DIALECT));
+		properties.put(PROPERTY_NAME_HIBERNATE_SHOW_SQL, env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_SHOW_SQL));
+		return properties;
+	}
+
+
+
+	@Bean
+	public UrlBasedViewResolver setupViewResolver() {
+		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+		resolver.setPrefix("/WEB-INF/pages/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		return resolver;
+	}
+	
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+		source.setBasename(env.getRequiredProperty("message.source.basename"));
+		source.setUseCodeAsDefaultMessage(true);
+		return source;
+	}
+    @Autowired
+    @Bean
+    public SessionFactory getSessionFactory(DataSource source) {
+        LocalSessionFactoryBuilder builder = new LocalSessionFactoryBuilder(source);
+        builder.scanPackages("com.medico.models");
+        builder.addProperties(hibProperties());
+        SessionFactory sessionFactory = builder.buildSessionFactory();
+        return sessionFactory;
+
+    }
+
+}
