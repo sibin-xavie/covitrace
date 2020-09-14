f9088521c209622dfe0ee39b91ded17ece297e2c
sibin <sibin.x@jeadigital.com>
2020-09-14 15:11:26 +0530

    changed the return type to LIST

--- covitrace/src/main/java/com/jea/medico/service/PatientServiceImpl.java
+++ covitrace/src/main/java/com/jea/medico/service/PatientServiceImpl.java
@@ -1,8 +1,15 @@
 package com.jea.medico.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.amazonaws.services.ec2.model.State;
+import com.jea.medico.model.MedicalDtlsModel;
+import com.jea.medico.model.StateModel;
 import com.jea.medico.model.UserChildModel;
 import com.jea.medico.model.UserMasterModel;
+import com.jea.medico.repository.StateRepository;
 /** 
 * 
 * @author Sibin 
@@ -10,70 +17,57 @@ import com.jea.medico.model.UserMasterModel;
 */
 public class PatientServiceImpl implements PatientService{
 
+	StateRepository stateRepo;
+	
+	 @Autowired
+     public PatientServiceImpl(StateRepository stateRepo) {
+         this.stateRepo = stateRepo;
+     }
+
 	@Override
-	public State getStateListService() {
+	public List<StateModel> getStateListService() {
+		// TODO Auto-generated method stub
+		return stateRepo.findAll();
+	}
+
+	
+
+	@Override
+	public List<UserMasterModel> authenticateUserService() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public State getDistrictListService() {
+	public List<UserChildModel> getStPatientListService() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public State getVillageListService() {
+	public List<UserMasterModel> getUserLastLogService() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public State getTalukListService() {
+	public List<UserChildModel> updatePatientService() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public UserMasterModel authenticateUserService() {
+	public List<MedicalDtlsModel> retrivePatMedQstHistService() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public UserChildModel getStPatientListService() {
+	public List<MedicalDtlsModel> getPatHealthDataService() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
-	public UserMasterModel getUserLastLogService() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	@Override
-	public UserChildModel updatePatientService() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public State retrivePatMedQstHistService() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public State getPatHealthDataService() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public State rerieveNotifService() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	
 }
