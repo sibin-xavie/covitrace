4ffff00413f788ecf907c741ccc7b690b0f3aaf2
sibin <sibin.x@jeadigital.com>
2020-09-16 11:01:44 +0530

    Added Controller

--- covitrace/src/main/java/com/jea/medico/controller/ViewController.java
+++ covitrace/src/main/java/com/jea/medico/controller/ViewController.java
@@ -1,14 +1,15 @@
 package com.jea.medico.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
+@Controller
 public class ViewController {
 
 	
-	 @RequestMapping(value = "/")
-	    public ModelAndView indexDirect() {
-	        ModelAndView view = new ModelAndView("index");
-	        return view;
-	    }
+	@RequestMapping(value = "/")
+    public ModelAndView indexDirect() {
+        ModelAndView view = new ModelAndView("index");
+        return view;
+    }
 }
