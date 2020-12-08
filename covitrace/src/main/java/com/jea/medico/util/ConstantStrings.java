a4f2c6a9a7bf6eb8d59a8b9eec0a4c433f352d20
sibin <sibin.x@jeadigital.com>
2020-12-08 17:34:59 +0530

    new Push

---
+++ covitrace/src/main/java/com/jea/medico/util/ConstantStrings.java
@@ -0,0 +1,13 @@
+package com.jea.medico.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConstantStrings {
+	
+	public String dataNotInserted = "Data Not Inserted";
+	public String duplicateEntry = "Duplicate entry found";
+	public String successString = "Success";
+	public String imageFile = "imageFile";
+
+}
